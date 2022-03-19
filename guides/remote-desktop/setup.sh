436b44b44ca89f58ce72dbbb6fc804bd7041353c
Felipe Escoto <felescoto95@hotmail.com>
2022-03-18 22:31:47 -0700

    Set flathub as the remote

--- guides/remote-desktop/setup.sh
+++ guides/remote-desktop/setup.sh
@@ -20,7 +20,7 @@ exit () {
 
 trap exit EXIT
 
-flatpak install org.kde.krfb -y
+flatpak install flathub org.kde.krfb -y
 
 # We need to give krfb access to the network, or else the service will not be available
 sudo flatpak override org.kde.krfb --share=network
